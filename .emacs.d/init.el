6821e0601c70f388156ad96c8a83b58838e95c13
Joshua Davey <josh@joshuadavey.com>
2017-05-25 19:49:34 +0000

    Add linum-format for terminal

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -104,6 +104,22 @@
 (add-hook 'clojure-mode-hook 'whitespace-mode)
 (add-hook 'clojure-mode-hook 'show-paren-mode)
 
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+            (lambda ()
+              (setq-local linum-format-fmt
+                          (let ((w (length (number-to-string
+                                            (count-lines (point-min) (point-max))))))
+                            (concat "%" (number-to-string w) "d"))))))
+
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-fmt line) 'face 'linum)
+   (propertize " " 'face 'mode-line)))
+
+(unless window-system
+  (setq linum-format 'linum-format-func))
+
 (defun cleanup-buffer ()
  "Perform a bunch of operations on the whitespace content of a buffer."
  (interactive)
