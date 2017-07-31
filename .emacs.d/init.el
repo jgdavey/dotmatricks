f26c7e33679d3054a246c994c201fc949448393f
Joshua Davey <josh@joshuadavey.com>
2017-07-30 21:34:49 -0400

    Add unfill-paragraph

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -320,6 +320,14 @@
 
 ; (setq sql-interactive-mode-hook nil)
 
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 ;; additional (local) config
 (dolist (extra-file '("~/.emacs.d/default.el" "~/.emacs.d/local.el"))
   (if (file-exists-p extra-file)
