c704ca233c877b2874bdf80d157c9734e6e94db8
Joshua Davey <josh@suiteness.com>
2018-03-07 10:12:06 -0500

    Exec path from shell in GUI emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -194,7 +194,9 @@
       (evil-set-initial-state `,(car mode-map) `,(cdr mode-map)))))
 
 (use-package exec-path-from-shell
-  :ensure t)
+  :ensure t
+  :init (when (memq window-system '(mac ns x))
+          (exec-path-from-shell-initialize)))
 
 (use-package expand-region
   :ensure t)
