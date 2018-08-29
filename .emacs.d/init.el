db0aef8a8ab0aa3d1a84dce730a5c1e884a0efee
Joshua Davey <josh@suiteness.com>
2018-08-29 10:51:10 -0400

    Add support for Co-authored-by in commit messages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -191,9 +191,17 @@
 
 (use-package magit
   :ensure t
-  :bind (("C-c g" . magit-status))
+  :bind (("C-c g" . magit-status)
+         :map git-commit-mode-map
+         ("C-c C-a" . git-commit-co-authored-by))
   :config
-  (setq magit-completing-read-function 'ivy-completing-read)
+  (add-to-list 'git-commit-known-pseudo-headers "Co-authored-by")
+  (defun git-commit-co-authored-by (name mail)
+    "Insert a header mentioning a co-author"
+    (interactive (git-commit-read-ident))
+    (git-commit-insert-header "Co-authored-by" name mail))
+  (setq magit-revision-show-gravatars '("^Author:     " . "^Commit:     ")
+        magit-completing-read-function 'ivy-completing-read)
   (magit-define-popup-switch 'magit-log-popup
     ?m "Omit merge commits" "--no-merges"))
 
