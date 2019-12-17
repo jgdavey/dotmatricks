c38ff7bb408b8e9341b355a590219d52a19fdb58
Joshua Davey <josh@joshuadavey.com>
2019-12-17 15:05:16 -0500

    Ruby emacs setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -142,7 +142,31 @@
 (use-package git-link
   :ensure t)
 
+(use-package inf-ruby
+  :ensure t)
+
 (use-package ruby-mode)
+
+(use-package enh-ruby-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist
+               '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)))
+
+(use-package robe
+  :ensure
+  :init
+  (add-hook 'ruby-mode-hook 'robe-mode)
+  (add-hook 'enh-ruby-mode-hook 'robe-mode))
+
+(use-package rspec-mode
+  :ensure t
+  ;; mimic rust mode mappings
+  :bind (:map rspec-mode-verifiable-keymap
+              ("C-t" . rspec-verify)
+              ("C-c" . rspec-rerun))
+  :init
+  (setq rspec-key-command-prefix (kbd "C-c C-C")))
 
 (use-package rust-mode
   :ensure t)
