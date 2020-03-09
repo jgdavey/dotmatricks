429bf5d9e5172d9676b67ac2ac6d5f4334dce095
Joshua Davey <josh@joshuadavey.com>
2020-03-09 11:50:49 -0400

    Add text scaling keyboard shortcuts for GUI emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -141,6 +141,15 @@
 (use-package expand-region
   :ensure t)
 
+(use-package default-text-scale
+  :ensure t
+  :bind (:map default-text-scale-mode-map
+              ("s-0" . default-text-scale-reset)
+              ("s-=" . default-text-scale-increase)
+              ("s--" . default-text-scale-decrease))
+  :init
+  (default-text-scale-mode))
+
 (use-package gist
   :ensure t)
 
