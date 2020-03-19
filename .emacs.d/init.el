48fa967071d50e20b9a0664b060040aa767b0237
Joshua Davey <josh@joshuadavey.com>
2020-03-19 15:52:41 -0400

    Ruby compilation and inf-ruby

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,13 +157,17 @@
   :ensure t)
 
 (use-package inf-ruby
-  :ensure t)
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'inf-ruby-switch-setup)
+  (add-hook 'compilation-filter-hook 'inf-ruby-auto-enter))
 
 (use-package ruby-mode)
 
 (use-package enh-ruby-mode
   :ensure t
   :init
+  (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)
   (add-to-list 'auto-mode-alist
                '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)))
 
