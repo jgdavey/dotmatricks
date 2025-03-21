949f245a4051cbcc7ba779e2695d9ede75d39c39
Joshua Davey <josh@joshuadavey.com>
2025-03-20 21:20:34 -0400

    Sync with sub upstream

--- jd/completions/jd.bash
+++ jd/completions/jd.bash
@@ -6,6 +6,7 @@ _jd() {
   if [ "$COMP_CWORD" -eq 1 ]; then
     completions="$(jd completions --commands | sed -e 's/\[.*//g')"
   else
+    export COMP_WORD="$word"
     completions="$(jd completions "${COMP_WORDS[1]}" "${COMP_WORDS[@]:2}" | sed -e 's/\[.*//g')"
   fi
   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
