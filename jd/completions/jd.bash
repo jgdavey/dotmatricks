bb774cd648ce11f1011eac8d1a97057fec645a58
Joshua Davey <josh@joshuadavey.com>
2026-05-27 23:04:03 -0400

    Use zub for jd

--- jd/completions/jd.bash
+++ jd/completions/jd.bash
@@ -1,16 +1,15 @@
-_jd() {
+_zub_complete() {
+  local prog="${COMP_WORDS[0]##*/}"
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
   local completions
-
   if [ "$COMP_CWORD" -eq 1 ]; then
-    completions="$(jd completions --commands | sed -e 's/\[.*//g')"
+    completions="$("$prog" completions --commands | sed -e 's/\[.*//g')"
   else
     export COMP_WORD="$word"
-    completions="$(jd completions "${COMP_WORDS[1]}" "${COMP_WORDS[@]:2}" | sed -e 's/\[.*//g')"
+    completions="$("$prog" completions "${COMP_WORDS[1]}" "${COMP_WORDS[@]:2}" | sed -e 's/\[.*//g')"
     unset COMP_WORD
   fi
   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
 }
-
-complete -F _jd jd
+complete -F _zub_complete jd
